56aa94322e6ddcd70f7dad8a1f96585fc8aa861d
Aravind K N <aravikn@gmail.com>
2016-03-17 23:34:48 +0530

    Update JavaApplication5.java

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -18,7 +18,8 @@ import java.util.List;
 
 class   MarksComparator implements Comparator<Student> {
         @Override
-        public int compare(Student a, Student b) {
+        public int compare(Student a, Student b) 
+        {
         return a.tot > b.tot ? -1 : a.tot == b.tot ? 0 : 1;
     }
 
@@ -29,20 +30,18 @@ class   MarksComparator implements Comparator<Student> {
 public class JavaApplication5 {
     
     
-    public static void main(String[] args) throws FileNotFoundException, IOException  
+    
+    public static ArrayList PrintAverage(CSVReader csvReader) throws IOException
     {
-        CSVReader csvReader = new CSVReader(new FileReader("C:\\Users\\ARAVIND\\Downloads\\input.csv"));
-        CSVWriter csvWriter = new CSVWriter(new FileWriter("C:\\Users\\ARAVIND\\Downloads\\output.csv"));
-     
         
-        ArrayList peps = new ArrayList();
+         
+        ArrayList stud_data = new ArrayList();
 
         String[] row = null;
         
         float avg1=0,avg2=0,avg3=0,avg;
         int count=0;//To get the total number of students
         int tot=0;// To calculate the total in each case
-        
         while((row = csvReader.readNext()) != null) 
         {
               tot=0;
@@ -61,66 +60,8 @@ public class JavaApplication5 {
               String total = Integer.toString(tot);
               String avrg = Float.toString(avg);
               
-              peps.add(new Student(row[0],row[1],row[2],row[3],row[4],tot,avrg));
+              stud_data.add(new Student(row[0],row[1],row[2],row[3],row[4],tot,avrg));
         }
-        
-         Collections.sort(peps,new MarksComparator());
-        
-        for (Iterator it = peps.iterator(); it.hasNext();) {
-            Student d = (Student) it.next();
-            csvWriter.writeAll(d.ToList());
-        }
-      
-        csvReader.close();
-       
-        csvWriter.close();
-        
-        
-        //To pront the top 5
-        
-        
-        CSVReader csvReadr = new CSVReader(new FileReader("C:\\Users\\ARAVIND\\Downloads\\output.csv"));
-        
-        
-        String rows[];
-        
-        int icount=0;
-        
-        System.out.println("The top 5 are");
-        
-        while((rows = csvReadr.readNext()) != null) 
-        {
-             if(icount<5)
-                 System.out.println(rows[0]+" "+rows[1]+" "+rows[2]+" "+rows[3]+" "+rows[4]+" "+rows[5]+" "+
-                         rows[6]);
-             icount++;
-             
-             if(icount>5)
-                 break;
-        }
-        csvReadr.close();
-        
-        //To print the bottom 2
-        
-        
-        String irow[];
-        
-        CSVReader csvRead = new CSVReader(new FileReader("C:\\Users\\ARAVIND\\Downloads\\output.csv"));
-        System.out.println("The bottom 2 are");
-        
-        icount=0;
-        
-        while((irow = csvRead.readNext()) != null) 
-        {
-             if(icount>=count-2)
-                 System.out.println(irow[0]+" "+irow[1]+" "+irow[2]+" "+irow[3]+" "+irow[4]+" "+irow[5]+" "+
-                         irow[6]);
-             icount++;
-        }
-        
-       csvRead.close();
-       
-        //To find the average
         
         avg1/=count;
         avg2/=count;
@@ -130,7 +71,92 @@ public class JavaApplication5 {
         System.out.println("The average for subject 1 is : "+avg1);
         System.out.println("The average for subject 2 is : "+avg2);
         System.out.println("The average for subject 3 is : "+avg3);
-} 
+        
+        csvReader.close();
+        
+        return stud_data;
+        
+        
+    }
+    
+    public static void Data_Sort(CSVWriter csvWriter, ArrayList stud_data) throws IOException
+    {
+        Collections.sort(stud_data,new MarksComparator());
+        
+        for (Iterator it = stud_data.iterator(); it.hasNext();) 
+        {
+            Student d = (Student) it.next();
+            csvWriter.writeAll(d.ToList());
+            
+        } 
+        
+        csvWriter.close();
+    }
+    
+    public static void TopN(CSVReader csvReadr) throws IOException
+    {
+        String rows[];
+        
+        int count=0;
+        
+        System.out.println("The top 5 are");
+        
+        while((rows = csvReadr.readNext()) != null) 
+        {
+             if(count<5)
+                 System.out.println(rows[0]+" "+rows[1]+" "+rows[2]+" "+rows[3]+" "+rows[4]+" "+rows[5]+" "+
+                         rows[6]);
+             count++;
+             
+             if(count>5)
+                 break;
+        }
+        csvReadr.close();
+    }
+    
+    public static void BotN(CSVReader csvRead) throws IOException
+    {
+      int count=0;
+      
+      String row[];
+        
+        System.out.println("The bottom 2 are");
+        
+        count=0;
+        
+        while((row = csvRead.readNext()) != null) 
+        {
+             if(count>=8)
+                 System.out.println(row[0]+" "+row[1]+" "+row[2]+" "+row[3]+" "+row[4]+" "+row[5]+" "+
+                         row[6]);
+             count++;
+        }
+        
+       csvRead.close();
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException  
+    {
+        CSVReader csvReader = new CSVReader(new FileReader("C:\\Users\\ARAVIND\\Downloads\\input.csv"));
+        CSVWriter csvWriter = new CSVWriter(new FileWriter("C:\\Users\\ARAVIND\\Downloads\\output.csv"));
+     
+        ArrayList stud_data;
+        
+        stud_data=PrintAverage(csvReader);
+ 
+        Data_Sort(csvWriter,stud_data);
+        
+        
+        CSVReader csvReadr = new CSVReader(new FileReader("C:\\Users\\ARAVIND\\Downloads\\output.csv"));
+        
+        TopN(csvReadr);
+        
+        CSVReader csvRead = new CSVReader(new FileReader("C:\\Users\\ARAVIND\\Downloads\\output.csv"));
+
+        BotN(csvRead);
+     
+        
+    } 
 }     
     
 
